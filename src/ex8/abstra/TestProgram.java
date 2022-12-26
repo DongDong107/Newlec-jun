2adc96d55cd12ee41614b8169031afa397432d15
unknown <jkds989@gmail.com>
2022-12-26 20:48:51 +0900

    Add 20221226 Summary

---
+++ src/ex8/abstra/TestProgram.java
@@ -0,0 +1,13 @@
+package ex8.abstra;
+
+public class TestProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Exam exam = new Exam(1,1,1);
+		
+		
+	}
+
+}
---
+++ src/ex9/wrapper/WrapperTest.java
@@ -0,0 +1,11 @@
+package ex9.wrapper;
+
+public class WrapperTest {
+
+	public static void main(String[] args) {
+		
+		Object obj = 3;
+
+	}
+
+}
