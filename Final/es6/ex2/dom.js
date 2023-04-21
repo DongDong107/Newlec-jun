c712b7b4e8456a4a053366c93be226f350a75088
DongDong107 <jkds989@gmail.com>
2023-04-21 18:11:47 +0900

    20230421 공부 : JS dom, animation

--- Final/es6/ex2/dom.js
+++ Final/es6/ex2/dom.js
@@ -1,7 +1,78 @@
 window.addEventListener('load', function () {
 
-  // 4. 이벤트 객체
-  {
+
+
+     // 4. 이벤트 객체
+     {
+      let s1 = document.querySelector(".s2");
+      let ul = s1.querySelector("ul");
+      let current = ul.querySelector("li.current");
+      let lis = ul.querySelectorAll("li");
+
+      for(let li of lis)
+          li.onclick = function(e){            
+
+              //* 현재 선택된 노드를 찾고
+              current = e.target.parentElement;
+              while(current.tagName !== "LI")
+                  current = current.parentElement;
+              
+              let selected = ul.querySelector("li.current");
+              if(selected){
+                console.log("2 번째 선택입니다.");
+                
+                // let temp = current;
+                
+                //   let tempNext = temp.nextElementSibling;
+                //   selected.insertAdjacentElement("afterend",temp);                
+                //   tempNext.insertAdjacentElement("beforebegin",selected);
+                
+
+                let temp = current;
+                if(temp.nextElementSibling){
+                  let tempNext = temp.nextElementSibling;
+                  selected.insertAdjacentElement("afterend",temp);                
+                  tempNext.insertAdjacentElement("beforebegin",selected);
+                }
+                else{
+                  ul.append(selected);
+                  ul.prepend(current);
+                }
+                
+                // selected.replaceWith(current, selected);
+
+                // current = selected;
+                // selected = temp;
+                // current <=> selected;  
+                // return;
+              }
+
+              // if(current){
+              //   console.log("두 번째 선택입니다.");
+
+              //   let selected = e.target.parentElement;
+              //   while(selected.tagName !== 'LI')
+              //     selected.e.parentElement;
+
+              //   let selectedBefore = selected.previousElementSibling;
+              //   current.replaceWith(selected);
+              //   selectedBefore.after(current);
+
+              // }
+                  
+              current.classList.toggle("current");
+              //* 선택된 노드임을 표시하는 스타일 변경
+              // let span = current.firstElementChild;
+              // while(span.tagName !== "SPAN")
+              //     span = span.firstElementChild;                 
+              // // 이렇게 스타일을 적용하는 것은 바람직하지 않다.
+              // span.style.backgroundColor = "red";
+              // console.log(current);
+          }
+  }
+
+    
+
     // 이벤트 종류
     // Event 객체의 속성
     // target / currentTarget
@@ -24,7 +95,70 @@ window.addEventListener('load', function () {
         //    console.log(e.currentTarget); -> B
         //}
 
-  }
+  // }
+
+
+  // {
+
+  //   let current = null;
+  //   let s2 = this.document.querySelector(".s2");
+
+  //   let lis = this.document.querySelectorAll(".s2 > ul > li");
+
+  //   for(let li of lis)
+  //     li.onclick = function(e){
+  //       // current = e.target;
+  //       current = e.target.parentElement;        
+  //       while(current.tagName !== "LI"){
+  //         current = current.parentElement;
+  //       }  
+
+  //       let span = current.firstElementChild;
+  //       while(span.tagName !== "SPAN")
+  //         span = span.firstElementChild;
+        
+  //       // for(let span = current.firstElementChild;
+  //       //   span.tagName !== "SPAN";
+  //       //   span = span.firstElementChild;)
+
+  //       // if(span.tagName !== "SPAN")
+
+  //       // span.style.backgroundColor = "red";
+
+  //       // 과연 current는 우리가 생각하듯이 li 일까?
+
+  //       console.log(current);
+
+  //       // 아니다. span이 나온다.
+  //       // 내가 생각한 이벤트 대상과 실제 이벤트 대상이 같은지 알고 진행해야 한다.
+
+  //     }
+
+    
+
+  //   // 이벤트 종류
+  //   // Event 객체의 속성
+  //   // target / currentTarget
+    
+  //           // Event 객체의 속성
+  //       // target / currentTarget
+  //       // ┌─────────┐
+  //       // │   A     │
+  //       // │ ┌─────┐ │
+  //       // │ │ B   │ │
+  //       // │ │     │ │
+  //       // 커서가 B를 클릭하면
+  //       // 이벤트가 발생한 녀석은 B이다.
+        
+  //       // A.onclick = function(e){
+  //       //    console.log(e.target); -> B
+  //       //    console.log(e.currentTarget); -> A
+  //       // B.onclick = function(e){
+  //       //    console.log(e.target); -> B
+  //       //    console.log(e.currentTarget); -> B
+  //       //}
+
+  // }
 
 
   // 3. 노드 조작을 위한 예제1
